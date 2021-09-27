57befa5bb0b4ec7965add9d99e539812815ef9cd
zackarno <zackarno@gmail.com>
2021-09-27 11:45:36 -0400

    initial ideas on some printing methods

---
+++ R/printing.R
@@ -0,0 +1,18 @@
+#' print methods for xlsf object
+#' special print for just data
+#' print for just survey data
+#' print for full objefct including data and questionnnaire
+
+print.xlsf_data <-  function(df){
+
+}
+print.xlsf_survey <- function(df){
+
+}
+
+print.xlsf <-  function(df){
+
+
+
+}
+
